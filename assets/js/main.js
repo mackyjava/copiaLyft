ce2130aa8288ca51d5ecfc02d2a0ed7ba7fbb763
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-04-26 14:05:44 -0500

    seccionmonse

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var video3=document.getElementById('tercerVideo');
+video3.addEventListener("click", reproducirvideo3);
+
+function reproducirvideo3(){
+  var imagenvideo3 = document.getElementById("imagensenor");
+  imagenvideo3.style.display= "none";
+  var video3= document.getElementById("video3");
+  video3.style.display="inline-block"
+}
